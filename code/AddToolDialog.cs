d0a6ebdeeb417588e48145ac6e2387b12c251c37
Alex <alex@gu3.me>
2022-11-01 16:09:27 +0000

    "Add tool" dialog

---
+++ code/AddToolDialog.cs
@@ -0,0 +1,130 @@
+﻿using Sandbox;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace Tools;
+
+public class AddToolDialog : Dialog
+{
+	[Menu( "Editor", "Tools Manager/Add Tool...", "add_box" )]
+	public static void OpenWindow()
+	{
+		_ = new AddToolDialog( null );
+	}
+
+	public AddToolDialog( Widget parent ) : base( null )
+	{
+		Window.Title = "Add Tool From GitHub";
+		Window.SetWindowIcon( "add_box" );
+		Window.Size = new Vector2( 500, 630 );
+		Window.MaximumSize = Size;
+
+		CreateUI();
+		Show();
+	}
+
+	public void CreateUI()
+	{
+		SetLayout( LayoutMode.TopToBottom );
+		Layout.Spacing = 0;
+		Layout.Margin = 0;
+
+
+		// Filtering
+		{
+			var filter = Layout.Add( LayoutMode.TopToBottom );
+			filter.Margin = new( 20, 20, 20, 0 );
+
+			filter.Add( new LineEdit( "" ) { PlaceholderText = "Search GitHub..." } );
+		}
+
+		// body
+		{
+			Layout.AddSpacingCell( 8 );
+			var AddonList = Layout.Add( new ListView(), 1 );
+			AddonList.ItemPaint = PaintAddonItem;
+			AddonList.ItemSize = new Vector2( 0, 38 );
+			Layout.AddSpacingCell( 8 );
+
+			var req = new HttpClient();
+			req.DefaultRequestHeaders.UserAgent.ParseAdd( "s&box" );
+			var content = req.GetAsync( "https://api.github.com/search/repositories?q=topic:sbox-tool" ).Result;
+			var result = content.Content.ReadAsStringAsync().Result;
+
+			var search = JsonSerializer.Deserialize<Search>( result );
+
+			foreach ( var item in search.Items )
+			{
+				AddonList.AddItem( item );
+			}
+		}
+
+		Layout.AddSeparator();
+
+		// Output
+		{
+			var lo = Layout.Add( LayoutMode.TopToBottom, 0 );
+			lo.Margin = 20;
+			lo.Spacing = 4;
+
+			lo.Add( new Label( "Check-out Location" ) );
+			var Location = lo.Add( new FolderProperty( null ) );
+			Location.Text = EditorPreferences.AddonLocation.NormalizeFilename( false );
+			Location.ToolTip = "This is where the addon will be downloaded.\n The folder will be created if it doesn't exist.";
+		}
+
+		Layout.AddSeparator();
+
+		// Footer buttons
+		{
+			var lo = Layout.Add( LayoutMode.LeftToRight );
+			lo.Margin = 20;
+			lo.Spacing = 4;
+
+			lo.AddStretchCell();
+			var OkayButton = lo.Add( new Button.Primary( "OK" ) );
+			lo.Add( new Button( "Cancel" ) { Clicked = Window.Close } );
+		}
+	}
+
+	private void PaintAddonItem( VirtualWidget v )
+	{
+		var rect = v.Rect;
+
+		if ( v.Object is not Item repo )
+			return;
+
+		Log.Trace( repo );
+
+		rect = rect.Shrink( 8, 0 );
+
+		Paint.Antialiasing = true;
+
+		Color fg = Theme.White.Darken( 0.2f );
+
+		if ( Paint.HasSelected )
+		{
+			fg = Theme.White;
+			Paint.ClearPen();
+			Paint.SetBrush( Theme.Primary.WithAlpha( 0.9f ) );
+			Paint.DrawRect( rect, 2 );
+
+			Paint.SetPen( Theme.White );
+		}
+		else
+		{
+			Paint.SetDefaultFont();
+			Paint.SetPen( Theme.Grey );
+		}
+
+		var textRect = rect.Shrink( 4 );
+
+		Paint.SetFont( "Poppins", 10, 450 );
+		Paint.SetPen( fg );
+		Paint.DrawText( textRect, repo.Name, TextFlag.LeftTop );
+
+		Paint.SetDefaultFont();
+		Paint.SetPen( fg.WithAlpha( 0.6f ) );
+		Paint.DrawText( textRect, repo.HtmlUrl, TextFlag.LeftBottom );
+	}
+}
---
+++ code/Types/Search.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Tools;
+public class Item
+{
+	[JsonPropertyName( "id" )]
+	public int Id { get; set; }
+
+	[JsonPropertyName( "node_id" )]
+	public string NodeId { get; set; }
+
+	[JsonPropertyName( "name" )]
+	public string Name { get; set; }
+
+	[JsonPropertyName( "full_name" )]
+	public string FullName { get; set; }
+
+	[JsonPropertyName( "private" )]
+	public bool Private { get; set; }
+
+	[JsonPropertyName( "owner" )]
+	public Owner Owner { get; set; }
+
+	[JsonPropertyName( "html_url" )]
+	public string HtmlUrl { get; set; }
+
+	[JsonPropertyName( "description" )]
+	public string Description { get; set; }
+
+	[JsonPropertyName( "fork" )]
+	public bool Fork { get; set; }
+
+	[JsonPropertyName( "url" )]
+	public string Url { get; set; }
+
+	[JsonPropertyName( "forks_url" )]
+	public string ForksUrl { get; set; }
+
+	[JsonPropertyName( "keys_url" )]
+	public string KeysUrl { get; set; }
+
+	[JsonPropertyName( "collaborators_url" )]
+	public string CollaboratorsUrl { get; set; }
+
+	[JsonPropertyName( "teams_url" )]
+	public string TeamsUrl { get; set; }
+
+	[JsonPropertyName( "hooks_url" )]
+	public string HooksUrl { get; set; }
+
+	[JsonPropertyName( "issue_events_url" )]
+	public string IssueEventsUrl { get; set; }
+
+	[JsonPropertyName( "events_url" )]
+	public string EventsUrl { get; set; }
+
+	[JsonPropertyName( "assignees_url" )]
+	public string AssigneesUrl { get; set; }
+
+	[JsonPropertyName( "branches_url" )]
+	public string BranchesUrl { get; set; }
+
+	[JsonPropertyName( "tags_url" )]
+	public string TagsUrl { get; set; }
+
+	[JsonPropertyName( "blobs_url" )]
+	public string BlobsUrl { get; set; }
+
+	[JsonPropertyName( "git_tags_url" )]
+	public string GitTagsUrl { get; set; }
+
+	[JsonPropertyName( "git_refs_url" )]
+	public string GitRefsUrl { get; set; }
+
+	[JsonPropertyName( "trees_url" )]
+	public string TreesUrl { get; set; }
+
+	[JsonPropertyName( "statuses_url" )]
+	public string StatusesUrl { get; set; }
+
+	[JsonPropertyName( "languages_url" )]
+	public string LanguagesUrl { get; set; }
+
+	[JsonPropertyName( "stargazers_url" )]
+	public string StargazersUrl { get; set; }
+
+	[JsonPropertyName( "contributors_url" )]
+	public string ContributorsUrl { get; set; }
+
+	[JsonPropertyName( "subscribers_url" )]
+	public string SubscribersUrl { get; set; }
+
+	[JsonPropertyName( "subscription_url" )]
+	public string SubscriptionUrl { get; set; }
+
+	[JsonPropertyName( "commits_url" )]
+	public string CommitsUrl { get; set; }
+
+	[JsonPropertyName( "git_commits_url" )]
+	public string GitCommitsUrl { get; set; }
+
+	[JsonPropertyName( "comments_url" )]
+	public string CommentsUrl { get; set; }
+
+	[JsonPropertyName( "issue_comment_url" )]
+	public string IssueCommentUrl { get; set; }
+
+	[JsonPropertyName( "contents_url" )]
+	public string ContentsUrl { get; set; }
+
+	[JsonPropertyName( "compare_url" )]
+	public string CompareUrl { get; set; }
+
+	[JsonPropertyName( "merges_url" )]
+	public string MergesUrl { get; set; }
+
+	[JsonPropertyName( "archive_url" )]
+	public string ArchiveUrl { get; set; }
+
+	[JsonPropertyName( "downloads_url" )]
+	public string DownloadsUrl { get; set; }
+
+	[JsonPropertyName( "issues_url" )]
+	public string IssuesUrl { get; set; }
+
+	[JsonPropertyName( "pulls_url" )]
+	public string PullsUrl { get; set; }
+
+	[JsonPropertyName( "milestones_url" )]
+	public string MilestonesUrl { get; set; }
+
+	[JsonPropertyName( "notifications_url" )]
+	public string NotificationsUrl { get; set; }
+
+	[JsonPropertyName( "labels_url" )]
+	public string LabelsUrl { get; set; }
+
+	[JsonPropertyName( "releases_url" )]
+	public string ReleasesUrl { get; set; }
+
+	[JsonPropertyName( "deployments_url" )]
+	public string DeploymentsUrl { get; set; }
+
+	[JsonPropertyName( "created_at" )]
+	public DateTime CreatedAt { get; set; }
+
+	[JsonPropertyName( "updated_at" )]
+	public DateTime UpdatedAt { get; set; }
+
+	[JsonPropertyName( "pushed_at" )]
+	public DateTime PushedAt { get; set; }
+
+	[JsonPropertyName( "git_url" )]
+	public string GitUrl { get; set; }
+
+	[JsonPropertyName( "ssh_url" )]
+	public string SshUrl { get; set; }
+
+	[JsonPropertyName( "clone_url" )]
+	public string CloneUrl { get; set; }
+
+	[JsonPropertyName( "svn_url" )]
+	public string SvnUrl { get; set; }
+
+	[JsonPropertyName( "homepage" )]
+	public string Homepage { get; set; }
+
+	[JsonPropertyName( "size" )]
+	public int Size { get; set; }
+
+	[JsonPropertyName( "stargazers_count" )]
+	public int StargazersCount { get; set; }
+
+	[JsonPropertyName( "watchers_count" )]
+	public int WatchersCount { get; set; }
+
+	[JsonPropertyName( "language" )]
+	public string Language { get; set; }
+
+	[JsonPropertyName( "has_issues" )]
+	public bool HasIssues { get; set; }
+
+	[JsonPropertyName( "has_projects" )]
+	public bool HasProjects { get; set; }
+
+	[JsonPropertyName( "has_downloads" )]
+	public bool HasDownloads { get; set; }
+
+	[JsonPropertyName( "has_wiki" )]
+	public bool HasWiki { get; set; }
+
+	[JsonPropertyName( "has_pages" )]
+	public bool HasPages { get; set; }
+
+	[JsonPropertyName( "forks_count" )]
+	public int ForksCount { get; set; }
+
+	[JsonPropertyName( "mirror_url" )]
+	public object MirrorUrl { get; set; }
+
+	[JsonPropertyName( "archived" )]
+	public bool Archived { get; set; }
+
+	[JsonPropertyName( "disabled" )]
+	public bool Disabled { get; set; }
+
+	[JsonPropertyName( "open_issues_count" )]
+	public int OpenIssuesCount { get; set; }
+
+	[JsonPropertyName( "license" )]
+	public License License { get; set; }
+
+	[JsonPropertyName( "allow_forking" )]
+	public bool AllowForking { get; set; }
+
+	[JsonPropertyName( "is_template" )]
+	public bool IsTemplate { get; set; }
+
+	[JsonPropertyName( "web_commit_signoff_required" )]
+	public bool WebCommitSignoffRequired { get; set; }
+
+	[JsonPropertyName( "topics" )]
+	public List<string> Topics { get; set; }
+
+	[JsonPropertyName( "visibility" )]
+	public string Visibility { get; set; }
+
+	[JsonPropertyName( "forks" )]
+	public int Forks { get; set; }
+
+	[JsonPropertyName( "open_issues" )]
+	public int OpenIssues { get; set; }
+
+	[JsonPropertyName( "watchers" )]
+	public int Watchers { get; set; }
+
+	[JsonPropertyName( "default_branch" )]
+	public string DefaultBranch { get; set; }
+
+	[JsonPropertyName( "score" )]
+	public double Score { get; set; }
+}
+
+public class License
+{
+	[JsonPropertyName( "key" )]
+	public string Key { get; set; }
+
+	[JsonPropertyName( "name" )]
+	public string Name { get; set; }
+
+	[JsonPropertyName( "spdx_id" )]
+	public string SpdxId { get; set; }
+
+	[JsonPropertyName( "url" )]
+	public string Url { get; set; }
+
+	[JsonPropertyName( "node_id" )]
+	public string NodeId { get; set; }
+}
+
+public class Owner
+{
+	[JsonPropertyName( "login" )]
+	public string Login { get; set; }
+
+	[JsonPropertyName( "id" )]
+	public int Id { get; set; }
+
+	[JsonPropertyName( "node_id" )]
+	public string NodeId { get; set; }
+
+	[JsonPropertyName( "avatar_url" )]
+	public string AvatarUrl { get; set; }
+
+	[JsonPropertyName( "gravatar_id" )]
+	public string GravatarId { get; set; }
+
+	[JsonPropertyName( "url" )]
+	public string Url { get; set; }
+
+	[JsonPropertyName( "html_url" )]
+	public string HtmlUrl { get; set; }
+
+	[JsonPropertyName( "followers_url" )]
+	public string FollowersUrl { get; set; }
+
+	[JsonPropertyName( "following_url" )]
+	public string FollowingUrl { get; set; }
+
+	[JsonPropertyName( "gists_url" )]
+	public string GistsUrl { get; set; }
+
+	[JsonPropertyName( "starred_url" )]
+	public string StarredUrl { get; set; }
+
+	[JsonPropertyName( "subscriptions_url" )]
+	public string SubscriptionsUrl { get; set; }
+
+	[JsonPropertyName( "organizations_url" )]
+	public string OrganizationsUrl { get; set; }
+
+	[JsonPropertyName( "repos_url" )]
+	public string ReposUrl { get; set; }
+
+	[JsonPropertyName( "events_url" )]
+	public string EventsUrl { get; set; }
+
+	[JsonPropertyName( "received_events_url" )]
+	public string ReceivedEventsUrl { get; set; }
+
+	[JsonPropertyName( "type" )]
+	public string Type { get; set; }
+
+	[JsonPropertyName( "site_admin" )]
+	public bool SiteAdmin { get; set; }
+}
+
+public class Search
+{
+	[JsonPropertyName( "total_count" )]
+	public int TotalCount { get; set; }
+
+	[JsonPropertyName( "incomplete_results" )]
+	public bool IncompleteResults { get; set; }
+
+	[JsonPropertyName( "items" )]
+	public List<Item> Items { get; set; }
+}
